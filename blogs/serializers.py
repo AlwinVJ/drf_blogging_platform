d5540ea6fd4025b00b2fecadf0e8683866e02328
AlwinVJ <alwinvj5@gmail.com>
2026-09-02 20:13:59 +0530

    feat: Customized category and author should be represented in an API

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
-from .models import Blog
+from .models import Blog, Category
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id','category']
+        
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username']
 
 class BlogSerializer(serializers.ModelSerializer):
+    
+    category = CategorySerializer(read_only = True) #Respresenting category relationship
+    author = UserSerializer(read_only = True) #Respresenting user relationship
     class Meta:
         model = Blog
         fields =    '__all__'
+
