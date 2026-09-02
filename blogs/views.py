415d67d6aa3a38f685ee2b40e02ae1f9677dc1a9
AlwinVJ <alwinvj5@gmail.com>
2026-09-02 13:05:57 +0530

    feat: Implemented blog list API

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Blog
+from .serializers import BlogSerializer
 
 # Create your views here.
+
+class BlogListAPIView(APIView):
+    
+    def get(self,request):
+        blogs = Blog.objects.all()
+        serializer = BlogSerializer(blogs,many = True)
+        
+        return Response(serializer.data)
